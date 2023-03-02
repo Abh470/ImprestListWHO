e01b5ea106b07018526dd9dd8b0c4896b4f95990
kunalkaushik987@gmail.com <kunalkaushik987@gmail.com>
2023-03-02 15:33:11 +0530

    First git commit for IPRS dashboard

---
+++ webparts/iprsDashboard/IprsDashboardWebPart.module.scss.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+require("./IprsDashboardWebPart.module.css");
+const styles = {
+  iprsDashboard: 'iprsDashboard_35cd6bd7',
+  teams: 'teams_35cd6bd7',
+  welcome: 'welcome_35cd6bd7',
+  welcomeImage: 'welcomeImage_35cd6bd7',
+  links: 'links_35cd6bd7'
+};
+
+export default styles;
+/* tslint:enable */
