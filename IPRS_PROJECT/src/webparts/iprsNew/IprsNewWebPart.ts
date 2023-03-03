fe228398c23b877cf08c2e34c030f50a1622a23b
Abh470 <abhishek.negi@titan4work.com>
2023-03-03 12:14:14 +0530

    New code

--- IPRS_PROJECT/src/webparts/iprsNew/IprsNewWebPart.ts
+++ IPRS_PROJECT/src/webparts/iprsNew/IprsNewWebPart.ts
@@ -1,7 +1,7 @@
 import { Version } from '@microsoft/sp-core-library';
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
+  PropertyPaneTextField 
 } from '@microsoft/sp-property-pane'; 
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 //import { IReadonlyTheme } from '@microsoft/sp-component-base';
